984a75edce3c90ba6dbc9b0f2bc77069616d7d87
Julia Gomes <juliagomes@Julias-MacBook-Pro.local>
2024-07-18 12:06:32 -0700

    Guard updates

--- benchmark_guard.py
+++ benchmark_guard.py
@@ -5,7 +5,7 @@
 import os
 import time
 from getpass import getpass
-from typing import List, Tuple
+from typing import List, Tuple, Type
 import logging
 
 import openai
@@ -13,7 +13,7 @@ import pandas as pd
 from sklearn.metrics import classification_report
 
 from guardrails import Guard
-from main import HallucinationPrompt, LlmRagEvaluator
+from main import HallucinationPrompt, QACorrectnessPrompt, ContextRelevancyPrompt, LlmRagEvaluator
 from phoenix.evals import download_benchmark_dataset
 
 logger = logging.getLogger(__name__)
@@ -21,6 +21,10 @@ logger = logging.getLogger(__name__)
 
 MODEL = "gpt-4-turbo"
 N_EVAL_SAMPLE_SIZE = 100
+# Choose one of HallucinationPrompt, QACorrectnessPrompt, ContextRelevancyPrompt
+PROMPT_TEMPLATE = HallucinationPrompt(prompt_name="hallucination_judge_llm")
+FAIL_RESPONSE = "hallucinated"
+PASS_RESPONSE = "factual"
 
 
 def evaluate_guard_on_dataset(test_dataset: pd.DataFrame, guard: Guard) -> Tuple[List[float], List[bool]]:
@@ -53,6 +57,16 @@ def evaluate_guard_on_dataset(test_dataset: pd.DataFrame, guard: Guard) -> Tuple
     return latency_measurements, guard_passed
 
 
+def get_benchmark_dataset(prompt_template: LlmRagEvaluator) -> pd.DataFrame:
+    if isinstance(prompt_template, HallucinationPrompt):
+        test_dataset = download_benchmark_dataset(task="binary-hallucination-classification", dataset_name="halueval_qa_data")
+    if isinstance(prompt_template, QACorrectnessPrompt):
+        test_dataset = download_benchmark_dataset(task="qa-classification", dataset_name="qa_generated_dataset")
+    if isinstance(prompt_template, ContextRelevancyPrompt):
+        test_dataset = download_benchmark_dataset(task="binary-relevance-classification", dataset_name="wiki_qa-train")
+    return test_dataset[:N_EVAL_SAMPLE_SIZE]
+
+
 if __name__ == "__main__":
     if not (openai_api_key := os.getenv("OPENAI_API_KEY")):
         openai_api_key = getpass("🔑 Enter your OpenAI API key: ")
@@ -60,15 +74,12 @@ if __name__ == "__main__":
     os.environ["OPENAI_API_KEY"] = openai_api_key
     
     # Columns: ['reference', 'query', 'response', 'is_hallucination']
-    test_dataset = download_benchmark_dataset(
-        task="binary-hallucination-classification",
-        dataset_name="halueval_qa_data")
-    test_dataset = test_dataset[:N_EVAL_SAMPLE_SIZE]
+    test_dataset = get_benchmark_dataset(prompt_template=PROMPT_TEMPLATE)
     
     guard = Guard.from_string(
         validators=[
             LlmRagEvaluator(
-                eval_llm_prompt_generator=HallucinationPrompt(prompt_name="hallucination_judge_llm"),
+                eval_llm_prompt_generator=PROMPT_TEMPLATE,
                 llm_evaluator_fail_response="hallucinated",
                 llm_evaluator_pass_response="factual",
                 llm_callable=MODEL,
