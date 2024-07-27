0b90fc2fbef30b5a2a019dcb2526d9f5cd27bafe
jgomes168 <168474583+jgomes168@users.noreply.github.com>
2024-07-27 09:46:20 -0700

    Juliagomes/fix init (#7)
    * imports
    
    * fix init
    
    ---------
    
    Co-authored-by: Julia Gomes <juliagomes@Julias-MacBook-Pro.local>

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-from .main import ValidatorTemplate
-
-__all__ = ["ValidatorTemplate"]
--- validator/__init__.py
+++ validator/__init__.py
@@ -1,3 +1,3 @@
-from .main import ValidatorTemplate
+from .main import LlmRagEvaluator
 
-__all__ = ["ValidatorTemplate"]
+__all__ = ["LlmRagEvaluator"]
